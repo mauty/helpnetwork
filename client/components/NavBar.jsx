3baf59e0242b221de4e0446a6870ce14ec0f73ea
Mike Auty <m@auty.ca>
2022-01-15 16:55:45 -0500

    linting fix

--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -5,32 +5,32 @@ const NavBar = (props) => {
   
   return (
     <ul>
-    <li>
-      <Link href="/">
-        <a>Home</a>
-      </Link>
-    </li>
-    <li>
-      <Link href="/points">
-        <a>Points</a>
-      </Link>
-    </li>
-    <li>
-      <Link href="/requests/new">
-        <a>New Request</a>
-      </Link>
-    </li>
-    <li>
-      <Link href="/messages">
-        <a>Messages</a>
-      </Link>
-    </li>
-    <li>
-      <Link href="/profile">
-        <a>Profile</a>
-      </Link>
-    </li>
-  </ul>
+      <li>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/points">
+          <a>Points</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/requests/new">
+          <a>New Request</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/messages">
+          <a>Messages</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/profile">
+          <a>Profile</a>
+        </Link>
+      </li>
+    </ul>
   );
 }
 
