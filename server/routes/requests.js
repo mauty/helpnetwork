528134a282c044fd94d5ec9aed4011b176428200
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-01-15 15:22:50 -0500

    🔥 Chore: Setup initial backend routes

--- server/routes/requests.js
+++ server/routes/requests.js
@@ -27,7 +27,7 @@ router.get('/requests', async function(req, res) {
 });
 
 /* POST request listing. */
-router.get('/request', async function(req, res) {
+router.post('/request', async function(req, res) {
   const { details, category, map_id, time_sensitive, start_time, end_time, points_value } = req.body;
 
   const request = await prisma.request.create({
@@ -40,11 +40,18 @@ router.get('/request', async function(req, res) {
       end_time,
       points_value,
       requester_id: 1 // TODO: USE cookie
+      // TODO: for resources, use an array coming from the body and create each in the requested resource
     }
   });
 
   res.json(request);
 });
+
+
+//TODO: for create profile page post resources
+//TODO: for profile display page get resources
+//TODO: for search filtering query by resources
+
 
 /* Offer help to request listing */
 router.post('/request/:request_id', async (req, res) => {
@@ -58,11 +65,17 @@ router.post('/request/:request_id', async (req, res) => {
 
   const conversation = await prisma.conversation.create({
     data: {
-      request_id
+      request: request.request_id,
+      requester_id: request.requester_id,
+      message: {
+        create: [
+          { body: `Hey I would love to help you with your request ${request.details}` }
+        ]
+      }
     }
   });
 
-  res.send(request);
+  res.send(conversation);
 });
 
 /*UPDATE request listing */
--- server/routes/users.js
+++ server/routes/users.js
@@ -6,6 +6,29 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/*Get route to see our own profile*/
+router.get('/profile', async function(req, res) {
+  // TODO: Get the ID from the session
+  const id = 1;
+
+  const person = await prisma.person.findOne({
+    where: { id }
+  });
+
+  res.json(person);
+});
+
+/*Get route to see other users profiles*/
+router.get('/profile/:id', async function(req, res) {
+  const { id } = req.params;
+
+  const person = await prisma.person.findOne({
+    where: { id }
+  });
+
+  res.json(person);
+});
+
 module.exports = router;
 
 
