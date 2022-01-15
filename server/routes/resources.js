566b00cd200fc7694e4deee66d893a5585116c8b
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-01-15 17:04:39 -0500

    👍 Fix: Changed requests to resources

--- server/routes/resources.js
+++ server/routes/resources.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const prisma = PrismaClient();
 
-/* GET request listing. */
+/* GET resources listing. */
 router.get('/resources', async function(_req, res) {
   const resources = await prisma.resource.findMany();
   res.json(resources);
