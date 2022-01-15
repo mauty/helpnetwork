162bff497426890346732b45a5e417ac370eee81
Mike Auty <m@auty.ca>
2022-01-15 16:50:04 -0500

    created Header and NavBar components, added links to home page

---
+++ client/components/NavBar.jsx
@@ -0,0 +1,37 @@
+import Link from 'next/link';
+
+const NavBar = (props) => {
+  const { } = props;
+  
+  return (
+    <ul>
+    <li>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+    </li>
+    <li>
+      <Link href="/points">
+        <a>Points</a>
+      </Link>
+    </li>
+    <li>
+      <Link href="/requests/new">
+        <a>New Request</a>
+      </Link>
+    </li>
+    <li>
+      <Link href="/messages">
+        <a>Messages</a>
+      </Link>
+    </li>
+    <li>
+      <Link href="/profile">
+        <a>Profile</a>
+      </Link>
+    </li>
+  </ul>
+  );
+}
+
+export default NavBar;
--- client/pages/index.js
+++ client/pages/index.js
@@ -3,35 +3,15 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 
+import Header from  '../components/Header';
+import NavBar from  '../components/NavBar';
+
 export default function Home() {
   return (
-    <ul>
-      <li>
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/points">
-          <a>Points</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/requests/new">
-          <a>New Request</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/messages">
-          <a>Messages</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/profile">
-          <a>Profile</a>
-        </Link>
-      </li>
-    </ul>
+    <>
+      <Header />
+      <NavBar />
+    </>
 
   )
 }
