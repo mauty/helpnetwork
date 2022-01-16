282c0748d2e3eebb7ce87ee8d9f6358a526eebbb
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-01-15 20:19:28 -0500

    🔥 Chore: Added dummy list

--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -1,21 +1,44 @@
-import * as React from "react";
-import { useState } from "react";
-import ReactMapGL from "react-map-gl";
+import { useState, useRef } from "react";
+import ReactMapGL, { GeolocateControl, NavigationControl } from "react-map-gl";
+import Mark from "./Map/Mark";
 
-export default function Map() {
+const geolocateControlStyle = {
+  right: 10,
+  top: 10,
+};
+
+const navControlStyle = {
+  right: 10,
+  bottom: 40,
+};
+
+export default function Map({ children }) {
+  const mapRef = useRef(null);
   const [viewport, setViewport] = useState({
-    width: 400,
-    height: 400,
     latitude: 37.7577,
     longitude: -122.4376,
-    zoom: 8,
+    zoom: 10,
   });
 
   return (
     <ReactMapGL
+      className="flex"
       {...viewport}
+      width="100%"
+      height="70vh"
       onViewportChange={(nextViewport) => setViewport(nextViewport)}
       mapboxApiAccessToken={`${process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}`}
-    />
+      mapStyle={"mapbox://styles/mapbox/streets-v11"}
+      ref={(instance) => (mapRef.current = instance)}
+    >
+      <Mark />
+      <GeolocateControl
+        style={geolocateControlStyle}
+        positionOptions={{ enableHighAccuracy: true }}
+        trackUserLocation={true}
+        auto
+      />
+      <NavigationControl style={navControlStyle} />
+    </ReactMapGL>
   );
 }
--- client/components/ui/Container.jsx
+++ client/components/ui/Container.jsx
@@ -1,10 +1,12 @@
+import clsx from "clsx";
+
 export default function Container({ title = "", size = "medium", children }) {
   return (
     <>
       {title}
       <div
         className={clsx(
-          "sm:mt-8 w-full mx-auto sm:rounded-lg shadow-lg bg-white dark:bg-gray-800 p-6",
+          "sm:mt-8 w-full mx-auto sm:rounded-lg shadow-lg bg-white dark:bg-gray-800 sm:p-6",
           {
             "sm:max-w-lg": size === "small",
             "sm:max-w-3xl": size === "medium",
@@ -12,16 +14,14 @@ export default function Container({ title = "", size = "medium", children }) {
           }
         )}
       >
-        {(title || action) && (
-          <div className="flex items-center justify-between mb-4">
-            {title && (
-              <h1 className="text-3xl font-bold text-gray-900 dark:text-gray-100">
-                {title}
-              </h1>
-            )}
-          </div>
-        )}
-        {/* {children} */}
+        <div className="flex items-center justify-between mb-4">
+          {title && (
+            <h1 className="text-3xl font-bold text-gray-900 dark:text-gray-100">
+              {title}
+            </h1>
+          )}
+        </div>
+        {children}
       </div>
     </>
   );
