9e284a075407a98eecc8d14091c84ccff4da8450
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-01-16 16:39:41 -0500

    🔥 Chore: Profile page

--- client/components/ui/Container.jsx
+++ client/components/ui/Container.jsx
@@ -3,10 +3,9 @@ import clsx from "clsx";
 export default function Container({ title = "", size = "medium", children }) {
   return (
     <>
-      {title}
       <div
         className={clsx(
-          "sm:mt-8 w-full mx-auto sm:rounded-lg shadow-lg bg-white dark:bg-gray-800 sm:p-6",
+          "sm:mt-8 w-full mx-auto sm:rounded-lg sm:shadow-lg bg-white dark:bg-gray-800 sm:p-6",
           {
             "sm:max-w-lg": size === "small",
             "sm:max-w-3xl": size === "medium",
@@ -16,7 +15,7 @@ export default function Container({ title = "", size = "medium", children }) {
       >
         <div className="flex items-center justify-between mb-4">
           {title && (
-            <h1 className="text-xl font-bold text-gray-900 dark:text-gray-100">
+            <h1 className="text-xl font-bold text-gray-900 dark:text-gray-100 p-2">
               {title}
             </h1>
           )}
