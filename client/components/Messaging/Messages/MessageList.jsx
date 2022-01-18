bfd8368edd8f63e062600194740d0f5e3c806631
Amir <amir.a.23@hotmail.com>
2022-01-18 02:20:53 +0000

    Add static messaging logic and ui improvements

--- client/components/Messaging/Messages/MessageList.jsx
+++ client/components/Messaging/Messages/MessageList.jsx
@@ -1,9 +1,30 @@
 import MessageListItem from "./MessageListItem";
 const MessageList = (props) => {
-  const { } = props;
+
+  //call db for list of conversations for current user
+  //map conversationListItem
+  //sort by timestamp, prepend
+  //link a conversation to a specific message
   
+  //at intervals of 5 seconds refresh component state to update conversation
+  //
+
+  const messages = [<div className="flex justify-center items-center mx-2 margin-top: 20px w-4/5 md:w-4/5 lg:w-4/5">    
+      <MessageListItem></MessageListItem>
+    </div>, <div className="flex justify-center items-center mx-2 margin-top: 20px w-4/5 md:w-4/5 lg:w-4/5">    
+      <MessageListItem></MessageListItem>
+    </div>, <div className="flex justify-center items-center mx-2 margin-top: 20px w-4/5 md:w-4/5 lg:w-4/5">    
+      <MessageListItem></MessageListItem>
+    </div>];
+
+const newArray = messages.map(
+  (arrayItem, arrayItemIndex, wholeArray) => arrayItem
+);
+    
   return (
-    <MessageListItem></MessageListItem>
+    <div>
+          {messages}
+    </div>
   );
 }
 
--- client/components/Messaging/Messages/MessageListItem.jsx
+++ client/components/Messaging/Messages/MessageListItem.jsx
@@ -2,8 +2,14 @@ const MessageListItem = (props) => {
   const { } = props;
   
   return (
-    <div><p>Hello My name is Amir.</p></div>
+    <div className="bg-white dark:bg-slate-900 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
+      <p className="text-slate-500 dark:text-slate-400 mt-2 text-sm">
+        Hello My name is Amir.
+      </p>
+    </div>
   );
 }
+
+
 
 export default MessageListItem;
