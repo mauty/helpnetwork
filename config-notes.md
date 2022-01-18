aa9d840c030a3296191de0e2335548ac16c6ad5c
Mike Auty <m@auty.ca>
2022-01-17 23:47:02 -0500

    added seed data for request_resource, personal_resource and conversations

--- config-notes.md
+++ config-notes.md
@@ -3,3 +3,10 @@
 1. Install VSCode extensions for both Eslint and Prettier
 2.
 3.
+
+## Database Commands
+
+While in /server directory:
+- npx prisma migrate reset
+- npx prisma migrate dev
+- npm run prisma:seed
--- server/prisma/seed.js
+++ server/prisma/seed.js
@@ -7,7 +7,7 @@ const person_data = [
     first_name: 'Bob',
     last_name: 'Johnson',
     bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus feugiat in ante metus dictum at tempor commodo. Pulvinar pellentesque habitant morbi tristique senectus et.",
-    postal_code: "M6B 1L9"
+    postal_code: "M6B 1L9",
     safety_details: 'Masks are good',
   },
   {
@@ -15,8 +15,19 @@ const person_data = [
     first_name: 'Sally',
     last_name: 'Smith',
     bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus feugiat in ante metus dictum at tempor commodo. Pulvinar pellentesque habitant morbi tristique senectus et.",
-    postal_code: "M4D 2U8"
+    postal_code: "M4D 2U8",
     safety_details: 'Masks are evil',
+  },
+  {
+    email: 'george@email.com',
+    first_name: 'George',
+    last_name: 'Hollinger',
+    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus feugiat in ante metus dictum at tempor commodo. Pulvinar pellentesque habitant morbi tristique senectus et.",
+    postal_code: "M5A T8E",
+    lat: 43.65818641884621,
+    long: -79.4805399308699,
+    safety_details: 'Masks, vaccines, boosted',
+
   },
 ];
 
@@ -39,8 +50,15 @@ const request_data = [
     request_details: 'I need a help clearing snow from my driveway',
     long: -79.3948,
     lat: 43.6677,
-    category_id: 3,
+    category_id: 4,
     requester_id: 1
+  },
+  {
+    request_details: 'I need help with cleaning my gutters. The downspouts are leaking.',
+    long: -79.1848,
+    lat: 43.8977,
+    category_id: 3,
+    requester_id: 2
   },
 ];
 
@@ -66,6 +84,23 @@ const resource_data = [
   { name: 'Ladder', category: 'Tool' },
   { name: 'Wheelbarrow', category: 'Tool' },
   { name: 'Hammer Drill', category: 'Tool' }
+];
+
+const requested_resource_data = [
+  { request_id: 3, resource_id: 3 },
+  { request_id: 2, resource_id: 0 },
+];
+
+const personal_resource_data = [
+  { person_id: 1, resource_id: 2},
+  { person_id: 0, resource_id: 4},
+  { person_id: 2, resource_id: 0},
+];
+
+const conversation_data = [
+  { helper_id: 2, requester_id: 1, request_id: 1 },
+  { helper_id: 1, requester_id: 2, request_id: 0 },
+  { helper_id: 0, requester_id: 3, request_id: 2 },
 ];
 
 const message_data = [
@@ -105,21 +140,21 @@ const main = async () => {
     data: resource_data,
   });
 
-  // await prisma.requested_resource.createMany({
-  // 	data: requested_resource_data,
-  // });
+  await prisma.requested_resource.createMany({
+    data: requested_resource_data,
+  });
 
-  // await prisma.personal_resource.createMany({
-  // 	data: personal_resource_data,
-  // });
+  await prisma.personal_resource.createMany({
+    data: personal_resource_data,
+  });
 
-  // await prisma.conversation.createMany({
-  // 	data: conversation_data,
-  // });
+  await prisma.conversation.createMany({
+    data: conversation_data,
+  });
 
-  // await prisma.message.createMany({
-  // 	data: message_data,
-  // });
+  await prisma.message.createMany({
+    data: message_data,
+  });
 
   console.log(`Seeding finished.`);
 };
