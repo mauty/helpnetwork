07e9150548d01c4a130d17aff52b008a56ab69a2
Mike Auty <m@auty.ca>
2022-01-20 14:33:33 -0500

    implements timeSensitive toggle and writes date/time info to state object

--- client/components/NewRequest/TimeChooser.jsx
+++ client/components/NewRequest/TimeChooser.jsx
@@ -1,27 +1,48 @@
 import { useState } from "react";
 import { set } from "react-hook-form";
+import { useContext } from "react";
+import { FormContext } from "../../contexts/FormContext";
 
 const TimeChooser = (props) => {
   const { } = props;
 
+  const { state, setState } = useContext(FormContext)
+
   const [timeSensitiveToggle, setTimeSensitiveToggle ] = useState(false)
   
-  const clickHandler = () => {
-    console.log('>>>> confirm time_sensitive = false');
-    // TO DO set state for time_senstive = false
+  const anytimeButton = () => {
+    setTimeSensitiveToggle(false)
+    setState((prevState) => ({...prevState, timeSensitive: false}))
+    updateDateState('')
+    updateTimeState('')
   }
+
+  const updateTimeSensitive = () => {
+    setTimeSensitiveToggle(!timeSensitiveToggle)
+    setState((prevState) => ({...prevState, timeSensitive: !timeSensitiveToggle}))
+    updateDateState('')
+    updateTimeState('')
+  }
+
+  const updateDateState = (dateInput) => {
+    setState((prevState) => ({...prevState, date: dateInput}));
+  };
+
+  const updateTimeState = (timeInput) => {
+    setState((prevState) => ({...prevState, startTime: timeInput}));
+  };
 
   return (
     <div className='flex justify-between p-2 gap-4 mt-8'>
       <div className="form-control gap-2 w-full">
-        <button className="btn btn-secondary mb-6" onClick={clickHandler}>Anytime is Fine</button>
+        <button className="btn btn-secondary mb-6" onClick={anytimeButton}>Anytime is Fine</button>
         <div className="flex justify-between items-center">
           <label className='text-lg font-semibold'>Or Specific Time:</label>
           <input
             type="checkbox" 
             className="toggle toggle-primary" 
-            unchecked="true" 
-            onChange={() => setTimeSensitiveToggle(!timeSensitiveToggle)}/>
+            checked={timeSensitiveToggle} 
+            onChange={updateTimeSensitive}/>
         </div>
         { timeSensitiveToggle &&
           <div id="date-time">
@@ -29,13 +50,21 @@ const TimeChooser = (props) => {
               <label className="label">
                 <span className="label-text">Date</span>
               </label>
-              <input type="date" className="input input-sm input-bordered w-full sm:w-80" placeholder="eg: M6C 2R8"/>
+              <input
+                type="date"
+                className="input input-sm input-bordered w-full sm:w-80"
+                onChange={(event) => updateDateState(event.target.value) }
+              />
             </div>
             <div>
               <label className="label">
                 <span className="label-text">Time</span>
               </label>
-              <input type="time" className="input input-sm input-bordered w-full sm:w-80" placeholder="eg: M6C 2R8"/>
+              <input
+                type="time"
+                className="input input-sm input-bordered w-full sm:w-80"
+                onChange={(event) => updateTimeState(event.target.value)}
+              />
             </div>
           </div>
         }
