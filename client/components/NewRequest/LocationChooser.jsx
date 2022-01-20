7f861a6e665bad4ae66cfef32d296915d5d939fd
Mike Auty <m@auty.ca>
2022-01-19 21:48:55 -0500

    add conditional render for postal code toggle

--- client/components/NewRequest/LocationChooser.jsx
+++ client/components/NewRequest/LocationChooser.jsx
@@ -10,14 +10,16 @@ const LocationChooser = (props) => {
   const [lng, setLng] = useState(null);
   const [status, setStatus] = useState(null);
 
+  const [locationToggle, setLocationToggle] = useState(false)
+
   const clickHandler = () => {
     console.log('>>>> trigger google places lookup');
     // TO DO google places geolocation lookup
   }
 
-  const getCoordsFromPostal = () => {
+  const getCoordsFromPostal = (fromForm) => {
     
-    Geocode.fromAddress("M4C3V5").then(
+    Geocode.fromAddress(fromForm).then(
       (response) => {
         const { lat, lng } = response.results[0].geometry.location;
         console.log('geocode>>>>',lat, lng);
@@ -26,6 +28,11 @@ const LocationChooser = (props) => {
         console.error(error);
       }
     );
+  }
+
+  const handleLocationClick = () => {
+    getLocation()
+    setLocationToggle(false)
   }
 
   const getLocation = () => {
@@ -47,20 +54,29 @@ const LocationChooser = (props) => {
   return (
     <div className='flex justify-between p-2 gap-4 mt-10'>
       <div className="form-control gap-2 w-full">
-      <button className="btn btn-secondary mb-6" onClick={getLocation}>Use My Current Location</button>
-      <h2>{lat}, {lng}</h2>
-      <div className="flex justify-between items-center">
-        <label className='text-lg font-semibold'>Or Somewhere Else:</label>
-        <input type="checkbox" className="toggle toggle-primary" unchecked="true" />
-      </div>
-        <label className="label">
-          <span className="label-text">Postal Code</span>
-        </label>
-        <input
-          type="text" 
-          className="input input-sm input-bordered w-full sm:w-80" 
-          placeholder="eg: M6C 2R8"
-          />
+        <button className="btn btn-secondary mb-6" onClick={handleLocationClick}>Use My Current Location</button>
+        <h2>{lat}, {lng}</h2>
+        <div className="flex justify-between items-center">
+          <label className='text-lg font-semibold'>Or Somewhere Else:</label>
+          <input
+            type="checkbox" 
+            className="toggle toggle-primary" 
+            unchecked="true" 
+            onChange={() => setLocationToggle(!locationToggle)}/>
+        </div>
+        { locationToggle &&
+          <div>
+            <label className="label">
+              <span className="label-text">Postal Code</span>
+            </label>
+            <input
+              type="text" 
+              className="input input-sm input-bordered w-full sm:w-80" 
+              placeholder="eg: M6C 2R8"
+              onChange={(event) => getCoordsFromPostal(event.target.value)}
+              />
+          </div>
+        }
       </div>
     </div>
   );
