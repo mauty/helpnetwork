66de13a752a6f691e5b4fc8de4b0aca895197e38
Amir <amir.a.23@hotmail.com>
2022-01-20 00:31:33 +0000

    Add message bubble alignment

--- client/components/Messaging/Messages/index.jsx
+++ client/components/Messaging/Messages/index.jsx
@@ -26,22 +26,21 @@ const MessageList = ({ messages = [] }) => {
   // }
   const listOfMessages = messages.map((message) => {
     return (
-      <MessageListItem
-        key={message.id}
-        id={message.id}
-        body={message.body}
-        timestamp={message.timestamp}
-        sender_id={message.sender_id}
-      />
+    //<div className={`${userID === receiver_userID ? "justify-right" : "justify-left"}`}>
+        <MessageListItem
+          key={message.id}
+          id={message.id}
+          body={message.body}
+          timestamp={message.timestamp}
+          sender_id={message.sender_id}
+        />
+    //</div>
+
     );
   });
 
   return (
-    /*
-    <div className={`${userID === receiver_userID ? "text-right" : "text-left"}`}>
-          {messages}
-    </div>
-    */
+  
     //list of messages
     <div className="">
       <div className="h-96 flex flex-col-reverse justify-end items-end divider overflow-y-scroll">
