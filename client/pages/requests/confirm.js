efbcecfda4e978e2ecf36468195e94ee05a3db70
Mike Auty <m@auty.ca>
2022-01-22 15:12:22 -0500

    added page for confirming help request

---
+++ client/pages/requests/confirm.js
@@ -0,0 +1,11 @@
+import NavBar from "../../components/NavBar";
+
+function Confirm() {
+  return (
+    <NavBar>
+      <h2>Thanks for submitting your help request</h2>
+    </NavBar>
+  )
+}
+
+export default Confirm
