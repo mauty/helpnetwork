15dd855a971a8840f9b989a7889f30a10f0f3764
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-01-23 18:52:12 -0500

    🔥 Chore: review hearts average

--- server/routes/reviews.js
+++ server/routes/reviews.js
@@ -4,8 +4,21 @@ const router = express.Router();
 const prisma = new PrismaClient();
 
 /*Get route to see other users profiles*/
-router.get('/profile/:id/newReview', async function (req, res) {
+router.post('/profile/:id/newReview', async function (req, res) {
+  const { body, userId } = req.body.params;
+  const rating = parseInt(req.body.params.rating);
+  const profileId = parseInt(req.params.id);
 
+  const newReview = await prisma.review.create({
+    data: {
+      body,
+      rating,
+      personId: profileId,
+      reviewerId: userId,
+    }
+  })
+
+  res.json(newReview);
 });
 
 module.exports = router;
