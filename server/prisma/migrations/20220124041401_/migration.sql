da40cea379e35e2da91910adf597b1a79c08cd4a
Mike Auty <m@auty.ca>
2022-01-24 10:25:32 -0500

    deleted old migrations, recreate first migration file

--- server/prisma/migrations/20220124041401_/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `category` on the `Resource` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Resource" DROP COLUMN "category",
-ADD COLUMN     "category_id" INTEGER NOT NULL DEFAULT 1;
