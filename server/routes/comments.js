7e7ed3095c79b89a44ca14eb85381a68ab037921
Amir <amir.a.23@hotmail.com>
2022-01-24 00:14:45 +0000

    Add posting comments route

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -3,11 +3,26 @@ const express = require('express');
 const router = express.Router();
 const prisma = new PrismaClient();
 
-/* GET categories listing. */
-router.get('/comments', async function (req, res) {
+/* GET comments. */
+router.get('/comments/:id', async function (req, res) {
 	const comments = await prisma.category.findMany();
 	console.log('hit comments route');
 	res.json(comments);
 });
 
+/* Post comment*/
+router.post('/comments/:id', async function (req, res) {
+	const { id } = req.params;
+	const { body, sender_id } = req.body.params;
+
+	const comments = await prisma.comment.create({
+		data: {
+			request_id: parseInt(id),
+			body,
+			sender_id,
+		},
+	});
+	console.log('hit comments route');
+	res.json(comments);
+});
 module.exports = router;
