6c22ef9ff3fe35b23bc02718be1c1a1e94ac1707
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-01-24 14:40:37 -0500

    🔥 Chore: Updated DesktopNav menus

--- client/components/ui/DesktopNav.jsx
+++ client/components/ui/DesktopNav.jsx
@@ -1,43 +1,27 @@
-/* This example requires Tailwind CSS v2.0+ */
-import { Fragment } from "react";
+import Link from "next/link";
+import { Fragment, useContext } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
-import { BellIcon, MenuIcon, XIcon } from "@heroicons/react/outline";
+import clsx from "clsx";
+import { UserContext } from "../../pages/_app";
 
-const user = {
-  name: "Tom Cook",
-  email: "tom@example.com",
-  imageUrl:
-    "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-};
 const navigation = [
-  { name: "Dashboard", href: "#", current: true },
-  { name: "Team", href: "#", current: false },
-  { name: "Projects", href: "#", current: false },
-  { name: "Calendar", href: "#", current: false },
-  { name: "Reports", href: "#", current: false },
-];
-const userNavigation = [
-  { name: "Your Profile", href: "#" },
-  { name: "Settings", href: "#" },
-  { name: "Sign out", href: "#" },
+  { name: "Home", href: "/", current: true },
+  { name: "Points", href: "/points", current: false },
+  { name: "Messages", href: "/messages", current: false },
 ];
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
-}
+const userNavigation = [
+  { name: "Your Profile", href: "/profile" },
+  { name: "Edit Profile", href: "/profile/edit" },
+  { name: "Sign out", href: "/auth/logout" },
+];
 
 export default function Example() {
+  const { currentUser } = useContext(UserContext);
+
   return (
     <>
-      {/*
-        This example requires updating your template:
-
-        ```
-        <html class="h-full bg-gray-100">
-        <body class="h-full">
-        ```
-      */}
-      <div className="min-h-full">
+      <div className="min-h-full sm:block hidden">
         <Disclosure as="nav" className="bg-gray-800">
           {({ open }) => (
             <>
@@ -54,43 +38,48 @@ export default function Example() {
                     <div className="hidden md:block">
                       <div className="ml-10 flex items-baseline space-x-4">
                         {navigation.map((item) => (
-                          <a
-                            key={item.name}
-                            href={item.href}
-                            className={classNames(
-                              item.current
-                                ? "bg-gray-900 text-white"
-                                : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                              "px-3 py-2 rounded-md text-sm font-medium"
-                            )}
-                            aria-current={item.current ? "page" : undefined}
-                          >
-                            {item.name}
-                          </a>
+                          <Link href={item.href}>
+                            <span
+                              key={item.name}
+                              className={
+                                "text-gray-300 hover:bg-gray-700 hover:text-white px-3 cursor-pointer py-2 rounded-md text-sm font-medium"
+                              }
+                              aria-current={item.current ? "page" : undefined}
+                            >
+                              {item.name}
+                            </span>
+                          </Link>
                         ))}
                       </div>
                     </div>
                   </div>
-                  <div className="hidden md:block">
-                    <div className="ml-4 flex items-center md:ml-6">
-                      <button
-                        type="button"
-                        className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                      >
-                        <span className="sr-only">View notifications</span>
-                        <BellIcon className="h-6 w-6" aria-hidden="true" />
-                      </button>
 
+                  <div className="hidden md:block">
+                    <div className="ml-4 flex items-center gap-10 md:ml-6">
+                      <Link href="/requests/new">
+                        <span
+                          className={clsx(
+                            "text-gray-100 hover:bg-gray-700 hover:text-white bg-primary px-3 cursor-pointer py-2 rounded-md text-sm font-medium"
+                          )}
+                        >
+                          <p>+ New Request</p>
+                        </span>
+                      </Link>
                       {/* Profile dropdown */}
                       <Menu as="div" className="ml-3 relative">
                         <div>
                           <Menu.Button className="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
                             <span className="sr-only">Open user menu</span>
-                            <img
-                              className="h-8 w-8 rounded-full"
-                              src={user.imageUrl}
-                              alt=""
-                            />
+                            {currentUser && currentUser.imgURL ? (
+                              <img
+                                src={currentUser.imgURL}
+                                className="rounded-full h-9 w-9"
+                              />
+                            ) : (
+                              <span className="rounded-full text-white border-white border px-4 py-1">
+                                ME
+                              </span>
+                            )}
                           </Menu.Button>
                         </div>
                         <Transition
@@ -102,19 +91,19 @@ export default function Example() {
                           leaveFrom="transform opacity-100 scale-100"
                           leaveTo="transform opacity-0 scale-95"
                         >
-                          <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                          <Menu.Items className="z-50 origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
                             {userNavigation.map((item) => (
                               <Menu.Item key={item.name}>
-                                {({ active }) => (
-                                  <a
-                                    href={item.href}
-                                    className={classNames(
-                                      active ? "bg-gray-100" : "",
-                                      "block px-4 py-2 text-sm text-gray-700"
-                                    )}
-                                  >
-                                    {item.name}
-                                  </a>
+                                {() => (
+                                  <Link href={item.href}>
+                                    <span
+                                      className={clsx(
+                                        "block cursor-pointer hover:bg-gray-200 px-4 py-2 text-sm text-gray-700"
+                                      )}
+                                    >
+                                      {item.name}
+                                    </span>
+                                  </Link>
                                 )}
                               </Menu.Item>
                             ))}
@@ -123,17 +112,20 @@ export default function Example() {
                       </Menu>
                     </div>
                   </div>
+
                   <div className="-mr-2 flex md:hidden">
                     {/* Mobile menu button */}
                     <Disclosure.Button className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
                       <span className="sr-only">Open main menu</span>
                       {open ? (
-                        <XIcon className="block h-6 w-6" aria-hidden="true" />
+                        <div></div>
                       ) : (
-                        <MenuIcon
-                          className="block h-6 w-6"
-                          aria-hidden="true"
-                        />
+                        // <XIcon className="block h-6 w-6" aria-hidden="true" />
+                        <div></div>
+                        // <MenuIcon
+                        //   className="block h-6 w-6"
+                        //   aria-hidden="true"
+                        // />
                       )}
                     </Disclosure.Button>
                   </div>
@@ -147,11 +139,8 @@ export default function Example() {
                       key={item.name}
                       as="a"
                       href={item.href}
-                      className={classNames(
-                        item.current
-                          ? "bg-gray-900 text-white"
-                          : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                        "block px-3 py-2 rounded-md text-base font-medium"
+                      className={clsx(
+                        "text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
                       )}
                       aria-current={item.current ? "page" : undefined}
                     >
@@ -159,63 +148,10 @@ export default function Example() {
                     </Disclosure.Button>
                   ))}
                 </div>
-                <div className="pt-4 pb-3 border-t border-gray-700">
-                  <div className="flex items-center px-5">
-                    <div className="flex-shrink-0">
-                      <img
-                        className="h-10 w-10 rounded-full"
-                        src={user.imageUrl}
-                        alt=""
-                      />
-                    </div>
-                    <div className="ml-3">
-                      <div className="text-base font-medium leading-none text-white">
-                        {user.name}
-                      </div>
-                      <div className="text-sm font-medium leading-none text-gray-400">
-                        {user.email}
-                      </div>
-                    </div>
-                    <button
-                      type="button"
-                      className="ml-auto bg-gray-800 flex-shrink-0 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                    >
-                      <span className="sr-only">View notifications</span>
-                      <BellIcon className="h-6 w-6" aria-hidden="true" />
-                    </button>
-                  </div>
-                  <div className="mt-3 px-2 space-y-1">
-                    {userNavigation.map((item) => (
-                      <Disclosure.Button
-                        key={item.name}
-                        as="a"
-                        href={item.href}
-                        className="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700"
-                      >
-                        {item.name}
-                      </Disclosure.Button>
-                    ))}
-                  </div>
-                </div>
               </Disclosure.Panel>
             </>
           )}
         </Disclosure>
-
-        <header className="bg-white shadow">
-          <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold text-gray-900">Dashboard</h1>
-          </div>
-        </header>
-        <main>
-          <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-            {/* Replace with your content */}
-            <div className="px-4 py-6 sm:px-0">
-              <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
-            </div>
-            {/* /End replace */}
-          </div>
-        </main>
       </div>
     </>
   );
